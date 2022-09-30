43b14b143789a6c24cccd162016c8f8e4d0d1e42
Leonardo Cristiano Quiosi <114519112+leoquiosi02@users.noreply.github.com>
2022-09-30 14:28:57 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+    
+    float nota1, nota2, nota3, nota4;
+
+    cout << "Digite a primeira nota: " << endl;
+    cin >> nota1;
+    cout << "Digite a segunda nota: " << endl;
+    cin >> nota2;
+    cout << "Digite a terceira nota: " << endl;
+    cin >> nota3;
+    cout << "Digite a quarta nota: " << endl;
+    cin >> nota4;
+    
+    float media = (nota1 + nota2 + nota3 + nota4) /4;
+    
+    cout << "Media Final: \n" << media << endl;
+    
+    if (media >= 60){
+    cout << "Aprovado" << endl;
+    }
+    else {
+    cout << "Reprovado" << endl;
+    }
+    
+    return 0;
+
+}
