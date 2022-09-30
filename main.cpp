51815e46bf945fa0bfc579f71eae94d922c38fd8
Leonardo Cristiano Quiosi <114519112+leoquiosi02@users.noreply.github.com>
2022-09-30 14:20:17 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+float valor_produto, valor_pago = 0.0;
+
+cout << "Digite o valor do produto: " << endl;
+cin >> valor_produto;
+cout << "Digite o valor pago: " << endl;
+cin >> valor_pago;
+
+float troco = valor_pago - valor_produto;
+
+if (troco > 0){
+cout << "O valor do produto e: " << troco << endl;
+}
+else {
+cout << "O valor pago e: " << -1 * troco << endl;
+}
+
+return 0;
+
+}
